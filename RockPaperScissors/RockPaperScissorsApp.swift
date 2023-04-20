877ce91b0909107b4bfeb4c19044f41ac77c7bb9
AskNilesh <rathodnilsrk@gmail.com>
2023-04-20 22:40:29 +0530

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Nilesh Rathod on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
